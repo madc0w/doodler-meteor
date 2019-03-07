8ccb230e06e9d610b2a4b86064ae4c1f541f3f21
Marc A. Donis <mad7@runbox.com>
2019-03-07 20:03:24 +0100

    .

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
-
-const black = "rgba(0, 0, 0, 255)";
-const white = "rgba(255, 255, 255, 255)";
+const black = "#000000";
+const white = "#ffffff";
 
 var canvas = null;
 var inputCanvas = null;
@@ -14,46 +13,11 @@ Template.main.helpers({
 });
 
 Template.main.events({
-	"change #contrast-range" : function(e) {
-		if (imageDimensions) {
-			const contrast = parseInt(e.target.value);
-			const cutoff = (1 << 8) * contrast / 100;
-			const imageData = inputContext.getImageData(0, 0, canvas.width, canvas.height);
-			var offset = 0;
-			for (var y = 0; y < imageData.height; y++) {
-				setProgress(y / imageData.height);
-				//				console.log("% done", (y / imageData.height) * 100);
-				for (var x = 0; x < imageData.width; x++) {
-					// turns out value is just max of R, G, B
-					const value = Math.max(
-						imageData.data[offset++],
-						imageData.data[offset++],
-						imageData.data[offset++]
-					);
+	"change #contrast-range" : updateContrast,
 
-					context.fillStyle = value < cutoff ? black : white;
-					context.fillRect(x, y, 1, 1);
-
-					//					// RGB
-					//					//					const offset = ((y * (imageData.width * 4)) + (x * 4));
-					//					const color = [
-					//						imageData.data[offset++],
-					//						imageData.data[offset++],
-					//						imageData.data[offset++]
-					//					];
-					//					color.alpha = imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 3];
-					offset++;
-				}
-			}
-			setProgress(1);
-		}
-	},
+	"change input[name='contrast-type']" : updateContrast,
 
 	"change #image-file-chooser" : function(e) {
-		//		
-		//	},
-		//	
-		//	"click #load-image-button" : function(e) {
 		if (typeof window.FileReader !== "function") {
 			alert("The file API isn't supported on this browser yet.");
 			return;
@@ -135,7 +99,66 @@ function rgb2hsv(color) {
 	return [ computedH, computedS, computedV ];
 }
 
-function setProgress(progress) {
-	$("#progress").css("width", (progress * 100) + "%");
-	$("#progress-container").css("display", progress >= 1 ? "none" : "block");
+//function setProgress(progress) {
+//	$("#progress").progressbar({
+//		value : progress * 100
+//	});
+//
+////	$("#progress").css("width", (progress * 100) + "%");
+////	$("#progress-container").css("display", progress >= 1 ? "none" : "block");
+//}
+
+function updateContrast() {
+	if (imageDimensions) {
+		//		context.fillStyle = value < cutoff ? black : white;
+		//		context.fillRect(0, 0, canvas.width, canvas.height);
+
+		const contrast = parseInt($("#contrast-range").val());
+		const cutoff = (1 << 8) * contrast / 100;
+		const imageData = inputContext.getImageData(0, 0, canvas.width, canvas.height);
+		const contrastType = $("[name='contrast-type']:checked").val();
+		var offset = 0;
+		for (var y = 0; y < imageData.height; y++) {
+			//				setProgress(y / imageData.height);
+			console.log("% done", (y / imageData.height) * 100);
+			for (var x = 0; x < imageData.width; x++) {
+				var value;
+				if (contrastType == "val") {
+					// turns out value is just max of R, G, B
+					value = Math.max(
+						imageData.data[offset++],
+						imageData.data[offset++],
+						imageData.data[offset++]
+					);
+				} else {
+					const color = {
+						red : imageData.data[offset++],
+						green : imageData.data[offset++],
+						blue : imageData.data[offset++]
+					};
+					const hsv = rgb2hsv(color);
+					if (contrastType == "sat") {
+						value = hsv[1];
+					} else if (contrastType == "hue") {
+						value = hsv[0];
+					//							console.log("hue", value);
+					}
+					value <<= 8;
+				}
+				context.fillStyle = value < cutoff ? black : white;
+				context.fillRect(x, y, 1, 1);
+
+				//					// RGB
+				//					//					const offset = ((y * (imageData.width * 4)) + (x * 4));
+				//					const color = [
+				//						imageData.data[offset++],
+				//						imageData.data[offset++],
+				//						imageData.data[offset++]
+				//					];
+				//					color.alpha = imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 3];
+				offset++;
+			}
+		}
+	//			setProgress(1);
+	}
 }
