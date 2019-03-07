cb87b18402076789e9f10df0c8e1efbc03fb1fa0
Marc A. Donis <mad7@runbox.com>
2019-03-07 17:13:55 +0100

    .

--- client/main.js
+++ client/main.js
@@ -10,14 +10,43 @@ Template.main.helpers({
 Template.main.events({
 	"change #contrast-range" : function(e) {
 		if (imageDimensions) {
-			console.log("context", context);
 
-			const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+			const black = "rgba(0,0,0,255)";
+			const white = "rgba(255,255,255,255)";
+
+			//			const black = context.createImageData(1, 1);
+			//			black.data = [ 0, 0, 0, 0 ];
+			//			const white = context.createImageData(1, 1);
+			//			white.data = [ (1 << 8) - 1, (1 << 8) - 1, (1 << 8) - 1, 0 ];
+
 			const contrast = parseInt(e.target.value);
+			const cutoff = (1 << 8) * contrast / 100;
+			const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+			//			console.log(imageData);
+			var offset = 0;
+			for (var y = 0; y < imageData.height; y++) {
+				console.log("% done", (y / imageData.height) * 100);
+				for (var x = 0; x < imageData.width; x++) {
+					// turns out value is just max of R, G, B
+					const value = Math.max(
+						imageData.data[offset++],
+						imageData.data[offset++],
+						imageData.data[offset++]
+					);
 
+					context.fillStyle = value < cutoff ? black : white;
+					context.fillRect(x, y, 1, 1);
+					//					context.putImageData(value < cutoff ? black : white, x, y);
 
-			for (var x = 0; x < imageDimensions.width; x++) {
-				for (var y = 0; x < imageDimensions.height; y++) {
+					//					// RGB
+					//					//					const offset = ((y * (imageData.width * 4)) + (x * 4));
+					//					const color = [
+					//						imageData.data[offset++],
+					//						imageData.data[offset++],
+					//						imageData.data[offset++]
+					//					];
+					//					color.alpha = imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 3];
+					offset++;
 				}
 			}
 		}
@@ -81,3 +110,25 @@ Template.main.onRendered(function() {
 	//	$("#output-canvas").width(innerWidth - $("#controls").width() - 42);
 	context = canvas.getContext("2d");
 });
+
+
+function rgb2hsv(color) {
+	r = color.red / 255;
+	g = color.green / 255;
+	b = color.blue / 255;
+	const minRGB = Math.min(r, g, b);
+	const maxRGB = Math.max(r, g, b);
+	const computedV = maxRGB;
+
+	// Black-gray-white
+	if (minRGB == maxRGB) {
+		return [ 0, 0, computedV ];
+	}
+
+	// Colors other than black-gray-white:
+	const d = r == minRGB ? g - b : (b == minRGB ? r - g : b - r);
+	const h = r == minRGB ? 3 : (b == minRGB ? 1 : 5);
+	const computedH = 60 * (h - d / (maxRGB - minRGB));
+	const computedS = (maxRGB - minRGB) / maxRGB;
+	return [ computedH, computedS, computedV ];
+}
